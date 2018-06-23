3460e5af791497afa9ae17c95f1be1f40b281c81
ud-youdie <ud.grandspring@gmail.com>
2018-06-23 14:07:54 +0900

    js微修正

--- js/camera.js
+++ js/camera.js
@@ -1,16 +1,14 @@
-(function(){
-  let localVideo = document.getElementById('display');
-  let localStream;
-  
-  // start local video
-  function startVideo() {
-    navigator.mediaDevices.getUserMedia({video: true, audio: false})
-    .then(function (stream) { // success
-      localStream = stream;
-      localVideo.src = window.URL.createObjectURL(localStream);
-    }).catch(function (error) { // error
-      console.error('mediaDevice.getUserMedia() error:', error);
-      return;
-    });
-  }
-})();
+let localVideo = document.getElementById('display');
+let localStream;
+
+// start local video
+function startVideo() {
+navigator.mediaDevices.getUserMedia({video: true, audio: false})
+.then(function (stream) { // success
+    localStream = stream;
+    localVideo.src = window.URL.createObjectURL(localStream);
+}).catch(function (error) { // error
+    console.error('mediaDevice.getUserMedia() error:', error);
+    return;
+});
+}
