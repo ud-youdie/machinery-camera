23282e602ec1e6acc8ebab084a8d6a97c4e2a322
ud-youdie <ud.grandspring@gmail.com>
2018-06-23 15:00:41 +0900

    canvasの描画を追加

--- js/camera.js
+++ js/camera.js
@@ -1,23 +1,36 @@
-let localVideo = document.getElementById('display');
-let localStream;
+let video = document.getElementById('display');
+let canvas = document.getElementById('#overlay');
+let context = canvas.getContext('2d');
 
-// start local video
-function startVideo() {
-    var medias = {
-        video: {
-            facingMode : {exact:"environment"}
-        },
-        audio: false
-    };
+var constraints = {
+    video: {
+        facingMode : {exact:"environment"}
+    },
+    audio: false
+};
 
-    navigator.mediaDevices.getUserMedia(medias)
-    .then(function (stream) { // success
-        localStream = stream;
-        localVideo.src = window.URL.createObjectURL(localStream);
-    }).catch(function (error) { // error
-        alert("カメラが使えないよ");
-        return;
-    });
-}
+navigator.mediaDevices.getUserMedia(constraints)
+.then(function (stream) { // success
+    video.src = window.URL.createObjectURL(stream);
+}).catch(function (error) { // error
+    alert("カメラが使えないよ");
+    return;
+});
 
-startVideo();
+video.addEventListener("loadedmetadata",function(e) {
+    //canvasにカメラの映像のサイズを設定
+    var ratio = window.innerWidth / video.videoWidth;
+    video.width = window.innerWidth;
+    video.height = video.videoHeight * ratio;
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    //getContextで描画先を取得
+    var ctx = canvas.getContext("2d");
+    //毎フレームの実行処理
+    setInterval(function(e) {
+        //videoタグの描画をコンテキストに描画
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(video,0,0,canvas.width,canvas.height);
+    },33);      
+});
