1136881d8300a6022e6784b1559a71234936cda1
ud-youdie <ud.grandspring@gmail.com>
2018-06-23 14:09:59 +0900

    js微修正

--- js/camera.js
+++ js/camera.js
@@ -3,12 +3,14 @@ let localStream;
 
 // start local video
 function startVideo() {
-navigator.mediaDevices.getUserMedia({video: true, audio: false})
-.then(function (stream) { // success
-    localStream = stream;
-    localVideo.src = window.URL.createObjectURL(localStream);
-}).catch(function (error) { // error
-    console.error('mediaDevice.getUserMedia() error:', error);
-    return;
-});
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    .then(function (stream) { // success
+        localStream = stream;
+        localVideo.src = window.URL.createObjectURL(localStream);
+    }).catch(function (error) { // error
+        console.error('mediaDevice.getUserMedia() error:', error);
+        return;
+    });
 }
+
+startVideo();
